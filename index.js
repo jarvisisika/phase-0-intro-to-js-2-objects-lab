daa7656ae8f7d346b535502b2dc19eb0f24e8803
jarvisisika <jarvis.isika@student.moringaschool.com>
2023-03-12 23:26:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+let employee = {
+    name: "John Smith",
+    streetAddress: "123 Main St"
+  };
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // return a new object with updated property
+    return { ...employee, [key]: value };
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // update property value in place
+    employee[key] = value;
+    // return updated object (optional)
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+    // create a new object with the specified key removed
+    const updatedEmployee = { ...employee };
+    delete updatedEmployee[key];
+    // return updated object
+    return updatedEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // delete property from original object
+    delete employee[key];
+    // return original object (optional)
+    return employee;
+  }
+  
